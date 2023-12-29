a1082f3516dbb92728fcf5c38ac922c695a3beb1
albi <aolo23@wp.pl>
2023-12-29 14:13:50 +0100

    1. Added tracing

--- onlinemed-backend/src/main/java/com/onlinemed/config/security/WebSecurityConfiguration.java
+++ onlinemed-backend/src/main/java/com/onlinemed/config/security/WebSecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,8 +64,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and().csrf().disable()
+        HttpSecurity httpSecurity = http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and().csrf().disable();
+
+        httpSecurity.authorizeRequests()
+                .antMatchers("/actuator").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .and();
+
+        httpSecurity
                 /* Add custom authorisation filter */
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class).authorizeRequests()
                 .antMatchers("/api/login/**").permitAll()
